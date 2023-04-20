fcd08f2f72d36acbe0ac468783ac12b6ad991e1a
bavanya <bkurra@ucsd.edu>
2023-04-20 13:12:44 -0700

    Update collatz.c

--- collatz/collatz.c
+++ collatz/collatz.c
@@ -1,5 +1,5 @@
 const char* USAGE_ERROR = "Usage %s starting_number\n";
-const char* OUT_OF_BOUNDS_ERRORS ="argument %lld must be >= 0 and less than 2,147,483,647\n";
+const char* OUT_OF_BOUNDS_ERRORS ="argument %lld must be > 0 and less than or equal to 2,147,483,647\n";
 const char* STOP_TIME = "n = %lld has a stopping time of %d\n";
 
 unsigned int
