2d67dc38fae7c35d29af0fa6fa50a5b39444bfc6
asaladi <122575267+asaladi@users.noreply.github.com>
2023-04-26 11:03:07 -0700

    Update collatz.c

--- collatz/collatz.c
+++ collatz/collatz.c
@@ -1,15 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 const char* USAGE_ERROR = "Usage %s starting_number\n";
 const char* OUT_OF_BOUNDS_ERRORS ="argument %lld must be > 0 and less than or equal to 2,147,483,647\n";
 const char* STOP_TIME = "n = %lld has a stopping time of %d\n";
 
 unsigned int
 compute(unsigned long long num){
-  //TODO: Implement the recursive Collatz 
+  if (num ==1){
+        return 0;
+  }
+  else if (num % 2 == 0){
+        return 1 + compute(num/2);
+  }
+  else{
+        return 1 + compute(num*3+1);
+  }
 }
 
 int
 main(int argc, char **argv){
-  //TODO: Implement input processing
+  if (argc !=2) {
+        fprintf(stderr, USAGE_ERROR, argv[0]);
+        return EXIT_FAILURE;
+  }
+  long long n = atoll( argv[1] );
 
-  //TODO: Get stopping time
+  if (n <= 0 || n>2147483647){
+        fprintf(stderr, OUT_OF_BOUNDS_ERRORS, n);
+        return EXIT_FAILURE;
+  }
+
+  printf(STOP_TIME, n, compute(n));
+
+  return EXIT_SUCCESS;
 }
