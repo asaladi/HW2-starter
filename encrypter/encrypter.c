6d92514e45f1e29316c9b4d5e32077f263edc890
asaladi <122575267+asaladi@users.noreply.github.com>
2023-04-26 11:00:47 -0700

    Update encrypter.c

--- encrypter/encrypter.c
+++ encrypter/encrypter.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_LEN 256 // maximum length of a single word
 
 int main(int argc, char* argv[]) {
-  char word[MAX_LEN];
 
-  FILE* fin = fopen( /* TODO: Read from input file */ );
+  if (argc !=3) {
+          fprintf(stderr, "Error, expected two arguments\n");
+          return EXIT_FAILURE;
+  }
+
+  // char word[MAX_LEN];
+
+  FILE* fin = fopen(argv[1], "r");
 
   if(fin == NULL) {
     perror("Could not open input file.");
@@ -15,6 +22,31 @@ int main(int argc, char* argv[]) {
   }
 
   // TODO: Write your code here to implement the encryption.
+
+  int num = atoi(argv[2]);
+
+  int cin;
+  int cout;
+  while ((cin = fgetc(fin)) != EOF){
+        if ( toupper(cin) >=65 && toupper(cin) <=90){
+
+                int extra;
+                if ( cin <= 90) {
+                        extra = 65;
+                }
+                else{
+                        extra = 97;
+                }
+
+                cout = ((cin-extra+num)%26)+extra;
+
+        }
+        else{
+                cout = cin;
+        }
+        fputc(cout, stdout);
+  }
+
 
   // clean up gracefully
   fclose(fin);
